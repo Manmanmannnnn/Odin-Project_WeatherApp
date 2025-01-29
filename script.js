9bde0c7d57ab2c420b950c45ca0b00cba3e0c98c
Manmanmannnnn <177126743+Manmanmannnnn@users.noreply.github.com>
2025-01-29 21:59:26 +0800

    add:design layout

--- script.js
+++ script.js
@@ -16,19 +16,62 @@ async function getData(){
     
 }
 
+function conditionValue(data){
+    const dataValue=data.trim().toLowerCase()
+    if(dataValue==='clear'){
+        return './images/clear_day_50dp_E8EAED_FILL0_wght400_GRAD0_opsz48.png'
+    }
+    else if(dataValue==='partially cloudy'){
+        return './images/partly_cloudy_day_50dp_E8EAED_FILL0_wght400_GRAD0_opsz48.png'
+
+    }
+    else if(dataValue==='cloudy'){
+        return './images/cloud_50dp_E8EAED_FILL0_wght400_GRAD0_opsz48.png'
+    }
+    else if(dataValue==='rain'){
+        return './images/rainy_50dp_E8EAED_FILL0_wght400_GRAD0_opsz48.png'
+    }
+    else return './images/foggy_50dp_E8EAED_FILL0_wght400_GRAD0_opsz48.png'
+    
+}
+
 async function updateOutput(){
     const data= await getData()
     console.log(data)
     output.innerHTML='';
 
     const title=document.createElement('h2');
-    title.textContent=  data.resolvedAddress;
+    title.textContent= `Location: ${data.resolvedAddress} ` ;
     output.append(title)
 
-    const condition=document.createElement('p');
+    const div=document.createElement('div');
+    div.classList.add('resultContainer')
+
+    const condition=document.createElement('div');
+    condition.classList.add('condition')
+
+    const currentCondition = await  data.currentConditions.conditions;
     
-    condition.textContent=  data.currentConditions.icon;
-    output.append(condition)
+    condition.textContent= currentCondition
+    div.append(condition)
+
+    console.log(conditionValue(currentCondition))
+    const img=document.createElement('img')
+    const imgSrc=conditionValue(currentCondition)
+    img.setAttribute('src',imgSrc);
+    condition.append(img)
+
+    loc.value=''
+   
+
+
+    const time=document.createElement('p');
+
+    time.textContent=  `Time: ${data.currentConditions.datetime}`;
+    time.classList.add('time')
+    div.append(time)
+
+    output.append(div)
 }
 
 search.addEventListener('click',()=>{
